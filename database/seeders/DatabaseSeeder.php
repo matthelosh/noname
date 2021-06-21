26d315b92fc349b001ec5827f0fc3361bda19571
Muhammad Soleh <matthelosh@gmail.com>
2021-06-22 01:51:10 +0700

    Add PeriodeSeedera4

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $files = ['menus', 'menu_role'];
+
+        for($i=0;$i<count($files);$i++) {
+            $path = public_path('files/'.$files[$i]);
+            $sql = file_get_contents($path);
+            DB::unprepared($sql);
+        }
     }
 }
