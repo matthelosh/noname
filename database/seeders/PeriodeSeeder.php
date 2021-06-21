5c52c8b51082666886fea2d0e5d3a5d0a6fe3e83
Muhammad Soleh <matthelosh@gmail.com>
2021-06-22 01:30:39 +0700

    Add PeriodeSeeder

---
+++ database/seeders/PeriodeSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Periode;
+
+class PeriodeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $periodes = [
+            [ 'kode_periode' => '20211', 'active' => 0, 'tapel' => '2020/2021', 'semester' => '1', 'deskripsi' => 'Periode 2020/2021 Semester Ganjil'],
+            [ 'kode_periode' => '20212', 'active' => 1, 'tapel' => '2020/2021', 'semester' => '2', 'deskripsi' => 'Periode 2020/2021 Semester Genap'],
+            [ 'kode_periode' => '21221', 'active' => 0, 'tapel' => '2021/2022', 'semester' => '1', 'deskripsi' => 'Periode 2021/2022 Semester Ganjil'],
+            [ 'kode_periode' => '21222', 'active' => 0, 'tapel' => '2021/2022', 'semester' => '2', 'deskripsi' => 'Periode 2021/2022 Semester Genap'],
+        ];
+
+        foreach($periodes as $periode) {
+            Periode::create([
+                'kode_periode' => $periode->kode_periode,
+                'tapel' => $periode->tapel,
+                'semester' => $periode->semester,
+                'deskripsi' => $periode->deskripsi,
+                'active' => $periode->active,
+            ]);
+        }
+    }
+}
