4ce5def4cdf087b13def87ee757a3106aff626fa
Muhammad Soleh <matthelosh@gmail.com>
2021-06-22 00:58:22 +0700

    Fix Login Page17

--- database/migrations/2021_06_19_130745_make_user_polymorphic.php
+++ database/migrations/2021_06_19_130745_make_user_polymorphic.php
@@ -14,10 +14,9 @@ class MakeUserPolymorphic extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            // $table->dropColumn('alamat', 'hp', 'jk', 'status','name');
-            $table->renameColumn('nip', 'userid');
-            // $table->integer('userable_id');
-            // $table->string('userable_type');
+            
+            $table->integer('userable_id');
+            $table->string('userable_type');
         });
     }
 
