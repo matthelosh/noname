f007bdc471f33c6ad5f70619a6a1e9deba10a7db
Muhammad Soleh <matthelosh@gmail.com>
2021-06-22 02:10:27 +0700

    Add PeriodeSeedera13

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,21 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $files = ['menus.sql', 'menu_role.sql'];
-        $db_bin = '/usr/bin/mysql';
-            $db = [
-               'username' => env('DB_USERNAME'),
-               'password' => env('DB_PASSWORD'),
-               'host' => env('DB_HOST'),
-               'database' => env('DB_DATABASE')
-           ];
-        for($i=0;$i<count($files);$i++) {
-            $path = public_path('files/'.$files[$i]);
-            // $sql = file_get_contents($path);
-            // DB::unprepared($sql);
-            
-           exec("{$db_bin} --user={$db['username']} --password={$db['password']} --host={$db['host']} --database {$db['database']} < $path");
-        }
+       $this->call([
+            UserSeeder::class,
+            AngkatanSeeder::class,
+            KelasSeeder::class,
+            MenuSeeder::class,
+            PeriodeSeeder::class,
+            RoleSeeder::class,
+       ]);
     }
 }
