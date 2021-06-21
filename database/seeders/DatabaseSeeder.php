6c112835521281ebbc0c4aed9bbb0b857f34bbe4
Muhammad Soleh <matthelosh@gmail.com>
2021-06-22 01:57:16 +0700

    Add PeriodeSeedera7

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,14 @@ class DatabaseSeeder extends Seeder
         for($i=0;$i<count($files);$i++) {
             $path = public_path('files/'.$files[$i]);
             $sql = file_get_contents($path);
-            DB::unprepared($sql);
+            // DB::unprepared($sql);
+            $db = [
+               'username' => env('DB_USERNAME'),
+               'password' => env('DB_PASSWORD'),
+               'host' => env('DB_HOST'),
+               'database' => env('DB_DATABASE')
+           ];
+           exec("{$db_bin}\mysql --user={$db['username']} --password={$db['password']} --host={$db['host']} --database {$db['database']} < $sql");
         }
     }
 }
