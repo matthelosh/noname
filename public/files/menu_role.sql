f65191baccb1e9069a05b499f5ba17c307dfdef9
Muhammad Soleh <matthelosh@gmail.com>
2021-06-22 01:51:57 +0700

    Add PeriodeSeedera5

---
+++ public/files/menu_role.sql
@@ -0,0 +1,90 @@
+-- Valentina Studio --
+-- MySQL dump --
+-- ---------------------------------------------------------
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-- ---------------------------------------------------------
+
+
+-- CREATE TABLE "menu_role" ------------------------------------
+CREATE TABLE `menu_role`( 
+	`id` BigInt( 20 ) UNSIGNED AUTO_INCREMENT NOT NULL,
+	`menu_id` VarChar( 255 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+	`role_id` VarChar( 255 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+	`created_at` Timestamp NULL DEFAULT NULL,
+	`updated_at` Timestamp NULL DEFAULT NULL,
+	PRIMARY KEY ( `id` ) )
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci
+ENGINE = InnoDB
+AUTO_INCREMENT = 58;
+-- -------------------------------------------------------------
+
+
+-- Dump data of "menu_role" --------------------------------
+BEGIN;
+
+INSERT INTO `menu_role`(`id`,`menu_id`,`role_id`,`created_at`,`updated_at`) VALUES 
+( '1', '1', '1', NULL, NULL ),
+( '2', '1', '2', NULL, NULL ),
+( '3', '1', '3', NULL, NULL ),
+( '4', '1', '4', NULL, NULL ),
+( '5', '1', '5', NULL, NULL ),
+( '6', '1', '6', NULL, NULL ),
+( '7', '2', '1', NULL, NULL ),
+( '8', '2', '2', NULL, NULL ),
+( '9', '2', '3', NULL, NULL ),
+( '10', '2', '4', NULL, NULL ),
+( '11', '2', '5', NULL, NULL ),
+( '12', '2', '6', NULL, NULL ),
+( '13', '3', '1', NULL, NULL ),
+( '14', '3', '2', NULL, NULL ),
+( '15', '4', '1', NULL, NULL ),
+( '16', '5', '1', NULL, NULL ),
+( '17', '6', '1', NULL, NULL ),
+( '18', '7', '1', NULL, NULL ),
+( '19', '8', '1', NULL, NULL ),
+( '20', '8', '2', NULL, NULL ),
+( '21', '9', '1', NULL, NULL ),
+( '22', '10', '1', NULL, NULL ),
+( '27', '10', '6', NULL, NULL ),
+( '28', '11', '2', NULL, NULL ),
+( '29', '11', '3', NULL, NULL ),
+( '30', '11', '4', NULL, NULL ),
+( '31', '11', '5', NULL, NULL ),
+( '32', '12', '2', NULL, NULL ),
+( '33', '12', '3', NULL, NULL ),
+( '34', '12', '4', NULL, NULL ),
+( '35', '12', '5', NULL, NULL ),
+( '36', '12', '2', NULL, NULL ),
+( '37', '13', '2', NULL, NULL ),
+( '38', '14', '1', NULL, NULL ),
+( '39', '15', '1', NULL, NULL ),
+( '40', '16', '1', NULL, NULL ),
+( '41', '17', '1', NULL, NULL ),
+( '42', '18', '1', NULL, NULL ),
+( '43', '19', '1', NULL, NULL ),
+( '44', '4', '2', NULL, NULL ),
+( '45', '4', '3', NULL, NULL ),
+( '46', '4', '4', NULL, NULL ),
+( '47', '4', '5', NULL, NULL ),
+( '49', '20', '2', NULL, NULL ),
+( '56', '21', '1', NULL, NULL ),
+( '57', '22', '1', NULL, NULL );
+COMMIT;
+-- ---------------------------------------------------------
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ---------------------------------------------------------
+
+
---
+++ public/files/menus.sql
@@ -0,0 +1,68 @@
+-- Valentina Studio --
+-- MySQL dump --
+-- ---------------------------------------------------------
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-- ---------------------------------------------------------
+
+
+-- CREATE TABLE "menus" ----------------------------------------
+CREATE TABLE `menus`( 
+	`id` BigInt( 20 ) UNSIGNED AUTO_INCREMENT NOT NULL,
+	`parent_id` Int( 11 ) NOT NULL DEFAULT 0,
+	`url` VarChar( 255 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+	`icon` VarChar( 255 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+	`label` VarChar( 255 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+	`created_at` Timestamp NULL DEFAULT NULL,
+	`updated_at` Timestamp NULL DEFAULT NULL,
+	PRIMARY KEY ( `id` ) )
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci
+ENGINE = InnoDB
+AUTO_INCREMENT = 23;
+-- -------------------------------------------------------------
+
+
+-- Dump data of "menus" ------------------------------------
+BEGIN;
+
+INSERT INTO `menus`(`id`,`parent_id`,`url`,`icon`,`label`,`created_at`,`updated_at`) VALUES 
+( '1', '0', '/dashboard', 'home', 'Beranda', NULL, NULL ),
+( '2', '0', '/dashboard/about', 'information', 'Tentang', NULL, NULL ),
+( '3', '0', '#', 'database', 'Data Master', NULL, NULL ),
+( '4', '0', '#', 'tablet-dashboard', 'Administrasi', NULL, NULL ),
+( '5', '0', '#', 'dip-switch', 'Pengaturan', NULL, NULL ),
+( '6', '0', '#', 'tools', 'Alat', NULL, NULL ),
+( '7', '3', '/dashboard/user', 'account-tie', 'Pengguna', NULL, NULL ),
+( '8', '3', '/dashboard/siswa', 'school', 'Siswa', NULL, NULL ),
+( '9', '3', '/dashboard/kelas', 'office-building', 'Kelas', NULL, NULL ),
+( '10', '3', '/dashboard/rombel', 'google-classroom', 'Rombel', NULL, NULL ),
+( '11', '4', '/dashboard/pembelajaran', 'bookshelf', 'Pembelajaran', NULL, NULL ),
+( '12', '4', '/dashboard/penilaian', 'check-box-multiple-outline', 'Penilaian', NULL, NULL ),
+( '13', '4', '/dashboard/rapor', 'book-open', 'Rapor', NULL, NULL ),
+( '14', '5', '/dashboard/sekolah', 'office-building-outline', 'Sekolah', NULL, NULL ),
+( '15', '5', '/dashboard/periode', 'calendar-month', 'Periode', NULL, NULL ),
+( '16', '5', '/dashboard/logs', 'notification-clear-all', 'Catatan Log', NULL, NULL ),
+( '17', '6', '/dashboard/backup', 'database-export', 'Cadangkan Data', NULL, NULL ),
+( '18', '6', '/dashboard/restore', 'database-import', 'Pulihkan Data', NULL, NULL ),
+( '19', '3', '/dashboard/mapel', 'book-open', 'Mapel', NULL, NULL ),
+( '20', '3', '/dashboard/prosem', 'notebook', 'Prosem', NULL, NULL ),
+( '21', '5', '/dashboard/pengaturan/rapor', 'book-open', 'Pengaturan Rapor', NULL, NULL ),
+( '22', '3', '/dashboard/guru', 'teach', 'Guru', NULL, NULL );
+COMMIT;
+-- ---------------------------------------------------------
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ---------------------------------------------------------
+
+
