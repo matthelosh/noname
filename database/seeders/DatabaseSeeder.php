87e2ff0e4ae4221523a4c5bbb1615a9b36885a6a
Muhammad Soleh <matthelosh@gmail.com>
2021-06-22 02:03:30 +0700

    Add PeriodeSeedera11

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,19 +16,19 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $files = ['menus.sql', 'menu_role.sql'];
-
-        for($i=0;$i<count($files);$i++) {
-            $path = public_path('files/'.$files[$i]);
-            $sql = file_get_contents($path);
-            // DB::unprepared($sql);
-            $db_bin = '/usr/bin/mysql';
+        $db_bin = '/usr/bin/mysql';
             $db = [
                'username' => env('DB_USERNAME'),
                'password' => env('DB_PASSWORD'),
                'host' => env('DB_HOST'),
                'database' => env('DB_DATABASE')
            ];
-           exec("{$db_bin} --user={$db['username']} --password={$db['password']} --host={$db['host']} --database {$db['database']} < $sql");
+        for($i=0;$i<count($files);$i++) {
+            $path = public_path('files/'.$files[$i]);
+            // $sql = file_get_contents($path);
+            // DB::unprepared($sql);
+            
+           exec("{$db_bin} --user={$db['username']} --password={$db['password']} --host={$db['host']} --database {$db['database']} < $path");
         }
     }
 }
