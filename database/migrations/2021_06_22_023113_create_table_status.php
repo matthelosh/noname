7f6790ee549232bc22dd41da16aa616020c04f5f
Muhammad Soleh <matthelosh@gmail.com>
2021-06-22 02:32:27 +0700

    Add PeriodeSeedera18

---
+++ database/migrations/2021_06_22_023113_create_table_status.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableStatus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_status', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('ok')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_status');
+    }
+}
--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -147,7 +147,7 @@ final class Versions
   'sebastian/type' => '2.3.1@81cd61ab7bbf2de744aba0ea61fae32f721df3d2',
   'sebastian/version' => '3.0.2@c6c1022351a901512170118436c764e473f6de8c',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'laravel/laravel' => 'dev-main@fe5372325171c34a1e045aba0333efced0b5215a',
+  'laravel/laravel' => 'dev-main@954e78f96da200756f2f7710c2453f82e6aeadeb',
 );
 
     private function __construct()
