7ae7a0936ed2d53d69342fa442703e80478d68ac
Muhammad Soleh <matthelosh@gmail.com>
2021-07-01 15:14:09 +0700

    Cek

---
+++ app/Http/Controllers/PembelajaranController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pembelajaran;
+use App\Models\Tema;
+use Illuminate\Http\Request;
+
+class PembelajaranController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        try {
+            // $datas = Pembelajaran::where('rombel_id', $request->rombel['kelas_id'])->get();
+            $rombel_id = $request->rombel['kode_rombel'];
+            // $datas = Tema::where([
+            //     ['kelas_id','=', $request->rombel['kelas_id']]
+            // ])->whereHas('subtemas.pembelajaran', function($q) use($rombel_id) {
+            //     $q->where('pembelajarans.rombel_id','=',$rombel_id);
+
+            // })->with('subtemas.pembelajaran')->get();
+
+            $datas = Tema::whereHas('pembelajaran', function($q) use ($rombel_id) {
+                $q->where('rombel_id','=', $rombel_id);
+            })->with('subtemas', function($s){
+                $s->whereHas('pembelajaran')->with('pembelajaran', function($p) {
+                    $p->with('guru', 'rombel');
+                });
+            })->orderBy('kode_tema')->get();
+            return response()->json(['success' => true, 'pembelajarans' => $datas], 200);
+        } catch (\Exception $e) {
+            dd($e);
+            // return response()->json(['success' => false, 'msg' => $e]);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            // kode Pemb: rombel_id:tematik:ke
+            Pembelajaran::updateOrCreate(
+                [
+                    'ke' => $request->ke,
+                    'kode_pembelajaran' => $request->rombel.':'.$request->tematik.':'.$request->subtema.':'.$request->ke,
+                    'periode_id' => $request->periode,
+                    'rombel_id' => $request->rombel,
+                    'guru_id' => $request->guru,
+                    'tematik' => $request->tematik,
+                    'tema_id' => $request->tema,
+                    'subtema_id' => $request->subtema,
+                ],
+                [
+                    'mupels' => $request->mupels,
+                    'tujuan' => $request->tujuan,
+                    'kegiatan' => $request->kegiatan,
+                    'penilaian' => $request->penilaian,
+                    'active' => 1,
+                    'tanggal' => $request->tanggal,
+                ]
+            );
+
+            return response()->json(['success' => true, 'msg' => 'Pembelajaran disimpan'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'msg' => $e]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pembelajaran  $pembelajaran
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pembelajaran $pembelajaran)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pembelajaran  $pembelajaran
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pembelajaran $pembelajaran)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pembelajaran  $pembelajaran
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pembelajaran $pembelajaran)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pembelajaran  $pembelajaran
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pembelajaran $pembelajaran)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PemetaanController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pemetaan;
+use Illuminate\Http\Request;
+
+class PemetaanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        try {
+            $datas = [];
+            if ( !$request->q ) {
+                $petas = Pemetaan::where([
+                    ['kelas_id','=', $request->kelas],
+                    ['tema_id','=', $request->tema],
+                    ['subtema_id','=', $request->subtema],
+                ])->with('mapel')->get();
+                foreach($petas as $peta)
+                {
+                    $kds = 'App\models\Kd'::where([
+                        ['kelas_id','=', $request->kelas],
+                        ['mapel_id','=', $peta->mapel_id],
+                        ['kode_kd', 'LIKE', '%'.substr($peta->kd_id, 1)]
+                    ])->get();
+                    $datas[] = ['id' => $peta->id, 'tema_id' => $peta->tema_id, 'subtema_id' => $peta->subtema_id, 'mapel_id' => $peta->mapel_id, 'mapel' => $peta->mapel, 'kd_id' => $peta->kd_id, 'kds' => $kds];
+                }
+            } else {
+                $semester = $request->semester;
+                $kelas = $request->kelas;
+                $temas = 'App\Models\Tema'::where([
+                    ['kelas_id' ,'=', $request->kelas]
+                ])->whereHas('subtemas.pemetaans', function($q) use ($semester){
+                    $q->where('pemetaans.semester','=',$semester);
+                        // ->with('kd', function($k) {
+                        //     $k->where();
+                        // });
+                })->with('subtemas.pemetaans', function($p)  use ($kelas){
+                    $kd = $p->get('kd_id')[0]['kd_id'];
+                    $p->with('kd', function($k) use ($kd, $kelas) {
+                        $k->where('kds.mapel_id','=',$k->get('mapel_id')[0]['mapel_id'])->where('kds.kelas_id','=',$kelas);
+                    })->with('mapel');
+                    
+                })->get();
+                $datas = $temas;
+            }
+            return response()->json(['success' => true, 'pemetaans' => $datas], 200);
+        } catch (\Exception $e) {
+            dd($e);
+            return response()->json(['success' => false, 'msg' => $e]);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pemetaan  $pemetaan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pemetaan $pemetaan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pemetaan  $pemetaan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pemetaan $pemetaan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pemetaan  $pemetaan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pemetaan $pemetaan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pemetaan  $pemetaan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pemetaan $pemetaan)
+    {
+        //
+    }
+}
